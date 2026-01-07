e8616d12637221af6103d77379ee09029871d6a4
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2026-01-07 22:18:09 +0530

    initial commit

---
+++ java_interview/reverse_string.java
@@ -0,0 +1,35 @@
+package java_interview;
+
+public class reverse_string {
+
+    // reverse string without builtin function
+    public static String reverse(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(chars);
+    }
+
+    // check palindrome
+    public static Boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
